ea580ad1cc4ae019d6a75c33f497c72d6a08b4b1
Roman Montoya <romanmontoya@Romans-MacBook-Pro-2.local>
2022-01-12 18:44:32 +0900

    update: test populate DB

--- app.py
+++ app.py
@@ -22,52 +22,18 @@ class Notes(db.Model):
     def __init__(self, content):
         self.content = content
 
-   
 
-
-db.create_all() 
-
-@app.route('/items/<id>', methods=['GET'])
-def get_item(id):
-    item = Notes.query.get(id)
-    del item.__dict__['_sa_instance_state']
-    return jsonify(item.__dict__)
-
-@app.route('/items', methods=['GET'])
-def get_items():
-    items = []
-    # for item in db.session.query(Notes).all():
-    #     del item.__dict__['_sa_instance_state']
-    #     items.append(item.__dict__)
-    return jsonify(items)
-    # return jsonify("the get method is possibly working")
-
-# @app.route('/items', methods=['POST'])
-# def create_item():
-#     body = request.get_json()
-#     db.session.add(Notes(body['content']))
-#     db.session.commit()
-#     return "item created"
-
-@app.route('/items/<id>', methods=['PUT'])
-def update_item(id):
-    body = request.get_json()
-    db.session.query(Notes).filter_by(id=id).update(
-        dict(content = body['content'])
-    )
-    db.session.commit()
-    return "item updated"
-
-@app.route('/items/<id>', methods=['DELETE'])
-def delete_item(id):
-    db.session.query(Notes).filter_by(id=id).delete()
-    db.session.commit()
-    return " item deleted"
 
 @app.route('/')
 def index():
+    #show all notes
+    # notes = Notes.query.all()
     return render_template('base.html')
 
+@app.route('/test')
+def message():
+    return "Hello"
 
 if __name__ == "__main__":
-      app.run(debug=True)  
+    # db.create_all()
+    app.run(debug=True)  
